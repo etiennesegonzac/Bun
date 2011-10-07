b846b937b19c80b48c5d7ea5b880c529559cb1af
Etienne Segonzac <etienne@segonzac.info>
2011-10-07 16:02:37 +0200

    Ready to screencat
    * 2 columns layout for medium size
    * Polish

--- prototypes/layout/responsive.js
+++ prototypes/layout/responsive.js
@@ -1,9 +1,26 @@
-$(document).delegate("a.projects-toggle", "click, touchend", function(event){
+$(document).delegate("a.projects-toggle", "click", function(event){
   $("#projects").toggleClass("shown");
+  $("#tasks").toggleClass("faded");
   event.preventDefault();
 });
 
-$(document).delegate(".inspector-toggle", "click, touchend", function(event){
+function inspectorHandle(event){
+  // window.scrollTo(0,0);
   $("#inspector").toggleClass("shown");
+  if (window.innerWidth <= 600) {
+    $("#tasks").toggleClass("faded");
+  }
   event.preventDefault();
+}
+$(document).delegate("a.inspector-toggle", "click", inspectorHandle);
+$(document).delegate("#tasks li", "click", inspectorHandle);
+
+$(document).ready(function(){
+  $("#tasks li").tap(inspectorHandle);
+  if (window.innerWidth > 600) {
+    $("#inspector").css('height', $('#tasks').height());
+    if (window.innerWidth > 1024) {
+      $("#projects").css('height', $('#tasks').height());
+    }
+  }
 });

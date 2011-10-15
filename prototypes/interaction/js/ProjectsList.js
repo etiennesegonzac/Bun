5bdc746170fe4a22fb2362989405975b9b6a673a
Etienne Segonzac <etienne@segonzac.info>
2011-10-15 19:34:24 +0200

    Apperently, the use of prototype is overkill for single-instance classes.

--- prototypes/interaction/js/ProjectsList.js
+++ prototypes/interaction/js/ProjectsList.js
@@ -1,14 +1,16 @@
+// We'll have only one instance
+
 function ProjectsList() {
   this.projects = [];
-}
 
-ProjectsList.prototype.load = function(){
-  // fake it till you make it
-  var fakeList = [{name: "Groceries", tasks: [{name: "Milk"}, {name: "eggs"}, {name:"bacon"}, {name: "Bacon"}, {name:"BACON"}]},
-                  {name: "Bun", tasks: [{name: "UI"}, {name: "IndexedDB"}, {name: "CouchDB"}, {name: "Sync"}]},
-                  {name: "B2G", tasks: [{name: "Make"}, {name: "it"}, {name: "awesome"}]}];
+  this.load = function() {
+    // fake it till you make it
+    var fakeList = [{name: "Groceries", tasks: [{name: "Milk"}, {name: "eggs"}, {name:"bacon"}, {name: "Bacon"}, {name:"BACON"}]},
+                    {name: "Bun", tasks: [{name: "UI"}, {name: "IndexedDB"}, {name: "CouchDB"}, {name: "Sync"}]},
+                    {name: "B2G", tasks: [{name: "Make"}, {name: "it"}, {name: "awesome"}]}];
 
-  fakeList.forEach(function(e) {
-    this.projects.push(new Project(e.name, e.tasks));
-  }, this);
+    fakeList.forEach(function(e) {
+      this.projects.push(new Project(e.name, e.tasks));
+    }, this);
+  }
 }
